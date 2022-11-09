bf454ce2cdf0956138bbfdb387ff71d5b2d4897a
seiya56 <76109619+seiya56@users.noreply.github.com>
2022-11-09 13:42:43 -0300

    Darkmode

--- JavaScript/Darkmode.js
+++ JavaScript/Darkmode.js
@@ -1,11 +1,50 @@
 const dark = document.getElementById('dark');
 
-dark.addEventListener('click', () => {
-    document.body.classList.toggle('darkmode')
-});
 const dark2 = document.getElementById('dark2');
 
-dark2.addEventListener('click', () => {
-    document.body.classList.toggle('darkmode')
+
+dark.addEventListener('click', () => {
+    document.body.classList.add('darkmode');
+
+    if(document.body.classList.contains('light')){
+
+        document.body.classList.remove('light');
+        
+        localStorage.setItem('Theme', 'darkmode');
+
+    }
+    else{
+        
+        document.body.classList.remove('darkmode');
+
+        document.body.classList.add('light');
+
+        localStorage.setItem('Theme', 'light')
+    };
+    
 });
+
+document.body.classList.add(localStorage.getItem('Theme'));
+
+
+dark2.addEventListener('click', () => {
+    document.body.classList.add('darkmode');
+    if(document.body.classList.contains('light')){
+        document.body.classList.remove('light');
+
+        localStorage.setItem('Theme', 'darkmode');
+
+    }
+    else{
+        
+        document.body.classList.remove('darkmode');
+
+        document.body.classList.add('light');
+
+        localStorage.setItem('Theme', 'light')
+    };
+    
+});
+
+
 
