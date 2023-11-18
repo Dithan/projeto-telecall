d278adf6809d521ecdb961457687b72d9d3690e0
Thiago Rocha <98711998+Dithan@users.noreply.github.com>
2023-11-18 11:44:14 -0300

    ADD: Autenticação de 2 fatores página

--- minha-conta/conta.php
+++ minha-conta/conta.php
@@ -8,7 +8,7 @@ if (isset($_SESSION["Usuario"])) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Admin - Tellecal</title>
+      <title>Conta - Tellecal</title>
       <link rel="stylesheet" href="../assets/css/style.css">
       <link rel="shortcut icon" href="../assets/media/Icons/favicon.ico" type="image/x-icon">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
