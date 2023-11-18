d719b0927fe89049df814c165a9747b3b9452d63
Thiago Rocha <98711998+Dithan@users.noreply.github.com>
2023-11-18 11:57:02 -0300

    ADD: Página Modelo de DB

---
+++ modelo-db.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telecall - Modelo do Banco de Dados</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="./assets/JavaScript/2fa.js"></script>
+    <link rel="shortcut icon" href="./assets/media/Icons/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <!-- Inclui Header -->
+    <?php include('./estrutura/header.php')?>
+
+    <section class="modelo-section">
+      <div class="container">
+        <h1 class="titulo">Modelo do Banco de Dados</h1>
+        <p>Imagem:</p>
+        <figure>
+          <img src="" alt="Modelo Banco de Dados">
+        </figure>
+      </div>
+    </section>
+
+    <script src="./assets/JavaScript/Darkmode.js"></script>
+</body>
+
+<!-- Inclui Footer -->
+<?php include('./estrutura/footer.php')?>
+
+</html>
